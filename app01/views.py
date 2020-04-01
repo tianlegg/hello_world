6636def0c238a81b1dcc05924c6a4dc5f3ce0473
litianle <tianlegg@gmail.com>
2020-04-01 22:33:54 +0800

    第二次提交

---
+++ app01/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,HttpResponse
+import datetime
+# Create your views here.
+
+def index(request):
+    now_time=datetime.datetime.now()
+    # return  HttpResponse(now_time)
+    return  render(request,'index.html',{'data':now_time})
