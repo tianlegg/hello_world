c1f3f9ff0ed9dd994eb0b0411e554ec392f8a00d
litianle <tianlegg@gmail.com>
2023-10-13 19:01:36 +0800

    2023年10月13日拉齐本地历史版本-修改日志输出位置

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -120,3 +120,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + '/logs/django.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
