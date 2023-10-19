9088e289ec740e73c944aba5c654507c1f66982f
Tianle <tianlegg@gmail.com>
2023-10-19 17:46:31 +0800

    Update settings.py

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -124,18 +124,30 @@ STATIC_URL = '/static/'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         },
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': '/app/debug.log',
+            'formatter': 'verbose'
         }
     },
     'root': {
-        'handlers': ['console'],
+        'handlers': ['console','file'],
         'level': 'DEBUG',
     },
 }
