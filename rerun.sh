6a910c3ffef298f519890e4239a1d5334d688a5c
Tianle <tianlegg@gmail.com>
2023-10-20 10:29:45 +0800

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -1,5 +1,10 @@
-git pull
-docker build -t django-env:test .
-docker rm -f django-demo
-docker run -itd -p 8000:8000 --name django-demo django-env:test
-docker logs django-demo
+#git pull
+#docker build -t django-env:test .
+#docker rm -f django-demo
+#docker run -itd -p 8000:8000 --name django-demo django-env:test
+#docker logs django-demo
+#BCE环境下，更新代码并构建docker镜像推送至BCE CCR
+cd /code
+while true;do git pull&&break;done
+docker build -t registry.baidubce.com/litianle/django-env:v0.12 .
+docker push registry.baidubce.com/litianle/django-env:v0.12
